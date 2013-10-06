1e49cb244fc0070c40dd162cb74f040c0f35cac5
David Schovanec <schovanec@schovi.cz>
2013-10-06 14:38:27 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :description "Clojure/Ring session store implemented on top of redis"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [ring/ring-core "1.2.0-beta2"]
                  [com.taoensso/carmine "2.2.1"]
